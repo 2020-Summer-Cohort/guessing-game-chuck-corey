0a98ead5cd5285a8b33202c1b0c2daf227ad95c6
Chuck R <rapmastercm@aol.com>
2020-05-15 09:57:48 -0400

    stored first integer

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -7,6 +7,17 @@ namespace GuessingGame
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Guessing Game!");
+            Console.WriteLine("Pick a number between 1 - 10");
+            int guess1 = Convert.ToInt32(Console.ReadLine());
+            int 
+          
+
+
+            Console.WriteLine("You Won");
+
+            //-1 exits the game 0 gives instructions
+
+
         }
     }
 }
