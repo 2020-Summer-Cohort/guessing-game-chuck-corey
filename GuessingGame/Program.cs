12a3b1fdba626156de92bd605cc8f30c0af75d13
Chuck R <rapmastercm@aol.com>
2020-05-15 15:09:06 -0400

    added some loops

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -8,29 +8,60 @@ namespace GuessingGame
         {
             Console.WriteLine("Welcome to Guessing Game!");
             Console.WriteLine("Pick a number between 1 - 10");
-            int guess1 = Convert.ToInt32(Console.ReadLine());           
-            if (guess1 == 7)
+            //int guess1 = Convert.ToInt32(Console.ReadLine());
+            //if (guess1 == 7)
+            //{
+            //    Console.WriteLine("You Won!");
+            //}
+            //else if (guess1 == 0)
+            //{
+            //    Console.WriteLine("You don't follow instructions. One More Guess");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("One More Guess");
+            //}
+
+            //int guess2 = Convert.ToInt32(Console.ReadLine());
+            //if (guess2 == 7)
+            //{
+            //    Console.WriteLine("You Won!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Game Over");
+
+            string guess1 = Console.ReadLine();
+            switch(guess1)
             {
-                Console.WriteLine("You Won!");
+
+                case "7":
+                    Console.WriteLine("You Won");
+                    break;
+                case "0":
+                    Console.WriteLine("You don't follow instructions. One more guess ");
+                    break;
+                case "-1":
+                    Console.Clear();//Console.WriteLine("Exit game"); //game should exit
+                    break;
+                default:
+                    Console.WriteLine("guess again");
+                    break;
             }
-            else if (guess1 == 0)
+            string guess2 = Console.ReadLine();
+            switch(guess2)
             {
-                Console.WriteLine("You don't follow instructions. One More Guess");
+                case "7":
+                    Console.WriteLine("You Won");
+                    break;
+                default:
+                    Console.WriteLine("game over");
+                    break;
+
             }
-            else
-            {
-                Console.WriteLine("One More Guess");
-            }
-            
-            int guess2 = Convert.ToInt32(Console.ReadLine());
-            if (guess2 == 7)
-            {
-                Console.WriteLine("You Won!");
-            }
-            else
-            {
-                Console.WriteLine("Game Over");
-            }
+
+
+
 
 
         }
