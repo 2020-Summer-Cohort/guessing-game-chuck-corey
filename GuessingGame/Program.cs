370f7daace5f2fff71328f12b55dc188391a5aad
Corey T Cox <coxcorey19@gmail.com>
2020-05-15 11:12:33 -0400

    added one more guess

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -8,14 +8,29 @@ namespace GuessingGame
         {
             Console.WriteLine("Welcome to Guessing Game!");
             Console.WriteLine("Pick a number between 1 - 10");
-            int guess1 = Convert.ToInt32(Console.ReadLine());
-            int 
-          
-
-
-            Console.WriteLine("You Won");
-
-            //-1 exits the game 0 gives instructions
+            int guess1 = Convert.ToInt32(Console.ReadLine());           
+            if (guess1 == 7)
+            {
+                Console.WriteLine("You Won!");
+            }
+            else if (guess1 == 0)
+            {
+                Console.WriteLine("You don't follow instructions. One More Guess");
+            }
+            else
+            {
+                Console.WriteLine("One More Guess");
+            }
+            
+            int guess2 = Convert.ToInt32(Console.ReadLine());
+            if (guess2 == 7)
+            {
+                Console.WriteLine("You Won!");
+            }
+            else
+            {
+                Console.WriteLine("Game Over");
+            }
 
 
         }
